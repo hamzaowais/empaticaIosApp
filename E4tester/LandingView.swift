d94f7899fd482c70d22323b8203c051811da5dd6
hamzaowais <hamza.agreed@gmail.com>
2018-07-23 14:58:01 -0400

    Dev

---
+++ E4tester/LandingView.swift
@@ -0,0 +1,39 @@
+//
+//  LandingView.swift
+//  E4tester
+//
+//  Created by Scott Pappada on 7/20/18.
+//  Copyright © 2018 Felipe Castro. All rights reserved.
+//
+
+import UIKit
+
+class LandingView: UIViewController {
+
+    @IBOutlet var ipAddress: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func Next(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
